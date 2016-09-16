e581d267d793302d830bc740a6895489696f2689
Linusp <linusp1024@gmail.com>
2016-09-16 22:27:03 +0800

    add base class for layers

--- connections/layers.py
+++ connections/layers.py
@@ -9,11 +9,25 @@ import numpy as np
 from connections.activations import get_activation
 
 
-class DenseLayer(object):
-    """经典的全连接层"""
+class Layer(object):
     def __init__(self, input_dim, output_dim, activation='linear'):
         self.input_dim = input_dim
         self.output_dim = output_dim
+        self.activation = get_activation(activation)
+
+        self.params = []
+
+    def get_params(self):
+        return self.params
+
+    def get_output_for(self, x):
+        raise NotImplementedError
+
+
+class DenseLayer(Layer):
+    """经典的全连接层"""
+    def __init__(self, input_dim, output_dim, activation='linear'):
+        super(DenseLayer, self).__init__(input_dim, output_dim, activation)
 
         W = theano.shared(np.random.randn(input_dim, output_dim), name='W')
         b = theano.shared(np.zeros((output_dim,)), name='b')
@@ -22,10 +36,8 @@ class DenseLayer(object):
         self.bias = b
         self.params = [self.weights, self.bias]
 
-        self.activation = get_activation(activation)
-
-    def get_params(self):
-        return self.params
+    def __repr__(self):
+        return '<DenseLayer({}, {})>'.format(input_dim, output_dim)
 
     def get_output_for(self, x):
         return self.activation(T.dot(x, self.weights) + self.bias)
